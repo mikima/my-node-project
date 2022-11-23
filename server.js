4fd44c339b0e803f9a210a0513679e759abff5ca
mikima <mikimauri@gmail.com>
2022-11-23 11:09:59 +0100

    set up public folder

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("hello hello");
+console.log("script loaded");
+
+let express = require("express");
+
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port);
+
+app.use(express.static("public"));
+
+console.log("running server on http://localhost:" + port);
