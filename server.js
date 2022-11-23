83b195083038f7b44ba13e84ce7391ddd76ca026
mikima <mikimauri@gmail.com>
2022-11-23 12:30:33 +0100

    added websockets

--- server.js
+++ server.js
@@ -11,3 +11,13 @@ let server = app.listen(port);
 app.use(express.static("public"));
 
 console.log("running server on http://localhost:" + port);
+
+let serverSocket = require("socket.io");
+
+let io = serverSocket(server);
+
+io.on("connection", newConnection);
+
+function newConnection(newSocket) {
+  console.log(newSocket.id);
+}
